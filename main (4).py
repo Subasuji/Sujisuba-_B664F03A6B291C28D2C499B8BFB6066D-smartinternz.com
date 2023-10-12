585d875431ee1b8287099510152c63dabd8cbc01
Subasuji <144584536+Subasuji@users.noreply.github.com>
2023-10-12 19:00:14 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,29 @@
+class BankAccount:
+  def __init__(self, account_number, account_holder_name,initial_balance=0.0):
+      self.__account_number = account_number
+      self.__account_holder_name = account_holder_name
+      self.__account_balance = initial_balance
+    
+  def deposit(self,amount):
+      if amount > 0:
+          self.__account_balance +=amount
+          print("Deposited ₹{}.New balance: ${}".format(amount,self.__account_balance)) 
+      else:  
+          print("Invalid deposit amount.")
+  def withdraw(self, amount):
+      if(amount > 0)and(amount <=                self.__account_balance):
+         self.__account_balance -= amount
+         print("withdraw ₹{}. New balance: ₹{}".format(amount,        self.__account_balance))
+      else:
+         print("Invalid withdraw amount             or insufficient balance.")
+  def display_balance(self):
+         print("Account balance for {}       (Account#{}): ₹{}".format(
+                self.__account_holder_name, self.__account_number,
+                self.__account_balance))
+myaccount = BankAccount(account_number = "817182331",
+    account_holder_name="Subashini", 
+    initial_balance=5000) 
+myaccount.display_balance()
+myaccount.deposit(500)
+myaccount.withdraw(20000)
+myaccount.display_balance()
